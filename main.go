713eb2d9cc444b8bfcedc9a16f5d5ab87f42e5ed
Sega <hpsergeyy@yandex.ru>
2025-08-24 16:50:59 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	log.Print("Запуск сервера...")
-
-	http.HandleFunc("/", routeHandler)
-	http.HandleFunc("/test", routeHandlerTest)
-
-	addr := ":8080"
-	log.Printf("Сервер запущен на %s", addr)
-
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		log.Fatalf("Ошибка запуска сервера: %v", err)
-	}
+	fmt.Println("Привет, Go!")
+	fmt.Println("w1w31231")
 }
+
+//import (
+//	"log"
+//	"net/http"
+//)
+//
+//func main() {
+//	log.Print("Запуск сервера...")
+//
+//	http.HandleFunc("/", routeHandler)
+//	http.HandleFunc("/test", routeHandlerTest)
+//
+//	addr := ":8080"
+//	log.Printf("Сервер запущен на %s", addr)
+//
+//	if err := http.ListenAndServe(addr, nil); err != nil {
+//		log.Fatalf("Ошибка запуска сервера: %v", err)
+//	}
+//}
