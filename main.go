51b340f33b735a475ee1b3220a5e8d106e843644
Sega <hpsergeyy@yandex.ru>
2025-08-24 18:59:03 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import "fmt"
 
-type Person struct {
+type Users struct {
+	Users []User
+}
+
+type Books struct {
+	Book []Book
+}
+
+type User struct {
 	Name   string
 	Age    uint8
 	Height float32
@@ -10,51 +18,87 @@ type Person struct {
 
 type Book struct {
 	Title  string
-	Author Person
 	Length uint16
 }
 
 func main() {
+	var user User
+	var users = Users{}
+	var books = Books{}
+
+	fmt.Println("Хочешь зарегистрироваться? y/n")
+
+	var goReg = "n"
+	fmt.Scan(&goReg)
+
+	if goReg == "y" {
+		user, users = makeUser(users)
+	} else {
+		user, users = makeUser(users)
+	}
+
+	for i, user := range users.Users {
+		fmt.Printf("Пользователь %d: %s, %d лет\n", i+1, user.Name, user.Age)
+	}
+
+	fmt.Printf("%s, спасибо, что ты мне написал. Тебе %d лет, а твой рост: %.2f см\n", user.Name, user.Age, user.Height)
+
+	books = addBook(books)
+
+	for i, book := range books.Book {
+		fmt.Printf("Книга %d: %s, %d стр.\n", i+1, book.Title, book.Length)
+	}
+}
+
+func makeUser(users Users) (User, Users) {
 	var name string
 	var age uint8
 	var height float32
 
-	fmt.Println("Привет, как тебя зовут?")
+	fmt.Println("Введи имя")
 	fmt.Scanln(&name)
 
-	fmt.Println("Отлично ", name, ", а сколько тебе лет?")
+	fmt.Println("Введи возраст")
 	fmt.Scan(&age)
 
-	fmt.Println("А рост?")
+	fmt.Println("Введи рост")
 	fmt.Scan(&height)
 
-	var person = Person{name, age, height}
+	var user = User{name, age, height}
 
-	fmt.Printf("%s, спасибо, что ты мне написал. Тебе %d лет, а твой рост: %.2f см\n", person.Name, person.Age, person.Height)
+	users.Users = append(users.Users, user)
 
+	return user, users
+}
+
+func addBook(books Books) Books {
 	fmt.Println("Хочешь добавить книгу? y/n")
 
 	var iaAddBook = "n"
-
 	fmt.Scan(&iaAddBook)
 
-	if iaAddBook == "y" {
-		var title string
-		var length uint16
-
-		fmt.Println("Как называется книга?")
-		fmt.Scanln(&title)
-
-		fmt.Println("Сколько в ней страниц")
-		fmt.Scan(&length)
-
-		var book = Book{title, person, length}
-
-		fmt.Printf("%s, спасибо что ты добавил книгу %s, количество страниц: %d \n", book.Title, book.Author.Name, book.Length)
-
-	} else {
-		fmt.Println("Как хочешь")
+	if iaAddBook != "y" {
+		return books
 	}
+
+	var title string
+	var length uint16
+
+	fmt.Println("Введи название книги")
+	fmt.Scanln(&title)
+
+	fmt.Println("Сколько количесво страниц")
+	fmt.Scan(&length)
+
+	var book = Book{title, length}
+
+	books.Book = append(books.Book, book)
+
+	fmt.Printf("Книга %s добавлена", book.Title)
+
+	addBook(books)
+
+	return books
 }
 
 //import (
